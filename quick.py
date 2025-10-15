9ecea8ec8f51315a23e2d07f5c18cb6a22deb367
Hari9306 <hariveera9306@gmail.com>
2025-10-15 22:15:53 +0530

    Update quick.py

--- quick.py
+++ quick.py
@@ -2,8 +2,8 @@ def quick_sort(arr):
     if len(arr) <= 1:
         return arr
     pivot = arr[0]
-    left = [x for x in arr if x < pivot]  # ❌ Includes pivot again
-    right = [x for x in arr if x > pivot]
+    left = [x for x in arr[1:] if x <= pivot]
+    right = [x for x in arr[1:] if x > pivot]
     return quick_sort(left) + [pivot] + quick_sort(right)
 
-print(quick_sort([3, 6, 8, 10, 1, 2, 1]))  # Expected [1, 1, 2, 3, 6, 8, 10]
+print(quick_sort([3, 6, 8, 10, 1, 2, 1]))
